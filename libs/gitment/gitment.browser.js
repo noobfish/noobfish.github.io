1aca1a93ed1b2a0b7e4fe8ea5337b8d555becb7f
Noobfish <noobfish@users.noreply.github.com>
2018-10-26 13:12:17 +0800

    Update gitment.browser.js

--- libs/gitment/gitment.browser.js
+++ libs/gitment/gitment.browser.js
@@ -3411,9 +3411,9 @@ var Gitment = function () {
         id: replacedUrl,
         link: replacedUrl
       }, options);
-//changed oauth server to https://bak.smalbox.club  , originally https://gh-oauth.imsun.net @by noobfish 2018.10.26
+// backup oauth server : https://bak.smalbox.club  @by noobfish 2018.10.26
       this.state.user.isLoggingIn = true;
-      _utils.http.post('https://bak.smalbox.club', {
+      _utils.http.post('https://gh-oauth.imsun.net', {
         code: code,
         client_id: client_id,
         client_secret: client_secret
